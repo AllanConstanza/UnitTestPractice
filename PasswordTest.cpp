4b85c9d0d9f539e907374d581081ec6f3c756809
AllanConstanza <allanconstanza2003@gmail.com>
2025-04-24 03:40:06 +0000

    Unit test submission

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,25 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, empty_string)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("");
+	ASSERT_EQ(1, actual);
+}
+
+
+TEST(PasswordTest, special_characters)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("aa$$##aaB"));
+}
+
+TEST(PasswordTest, repeating_leading_characters)
+{
+	Password my_password;
+	ASSERT_EQ(7, my_password.count_leading_characters("aaaaaaab"));
+}
+
+
